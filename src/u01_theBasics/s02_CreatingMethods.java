37cafae32d59c9841cbce245231f006058f49ac3
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2023-08-30 09:24:31 -0500

    Partial Implementation of Methods

--- src/u01_theBasics/s02_CreatingMethods.java
+++ src/u01_theBasics/s02_CreatingMethods.java
@@ -13,9 +13,11 @@ public class s02_CreatingMethods {
         //Mouth
         StdDraw.setPenColor(StdDraw.RED);
         StdDraw.filledEllipse(75,65,10,3);
-        drawEyes();
 
-        drawCircles();
+        drawEyes();
+        drawCircles(25,25);
+        drawCircles(25,75);
+        drawCircles(75,25);
     }
 
     public static void drawEyes(){
@@ -28,9 +30,9 @@ public class s02_CreatingMethods {
     }
 
     public static void drawCircles(){
-        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.setPenRadius(.05);
-        StdDraw.circle(25,25,10);
+        StdDraw.circle(xCoord,yCoord,10);
         StdDraw.setPenColor(StdDraw.RED);
         StdDraw.setPenRadius(.05/2);
         StdDraw.circle(25,25,5);
@@ -43,6 +45,39 @@ public class s02_CreatingMethods {
         StdDraw.setPenColor(StdDraw.CYAN);
         StdDraw.setPenRadius(.05/16);
         StdDraw.circle(25,25,.75);
+
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(.05);
+        StdDraw.circle(25,75,10);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.setPenRadius(.05/2);
+        StdDraw.circle(25,75,5);
+        StdDraw.setPenColor(StdDraw.GREEN);
+        StdDraw.setPenRadius(.05/4);
+        StdDraw.circle(25,75,2.5);
+        StdDraw.setPenColor(StdDraw.MAGENTA);
+        StdDraw.setPenRadius(.05/8);
+        StdDraw.circle(25,75,1.25);
+        StdDraw.setPenColor(StdDraw.CYAN);
+        StdDraw.setPenRadius(.05/16);
+        StdDraw.circle(25,75,.75);
+
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(.05);
+        StdDraw.circle(75,25,10);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.setPenRadius(.05/2);
+        StdDraw.circle(75,25,5);
+        StdDraw.setPenColor(StdDraw.GREEN);
+        StdDraw.setPenRadius(.05/4);
+        StdDraw.circle(75,25,2.5);
+        StdDraw.setPenColor(StdDraw.MAGENTA);
+        StdDraw.setPenRadius(.05/8);
+        StdDraw.circle(75,25,1.25);
+        StdDraw.setPenColor(StdDraw.CYAN);
+        StdDraw.setPenRadius(.05/16);
+        StdDraw.circle(75,25,.75);
+
     }
 
 }
